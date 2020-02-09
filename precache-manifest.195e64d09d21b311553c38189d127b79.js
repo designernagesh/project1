d6d4a3ce70cce323e0fa29ec3e53ee9bdb8376d9
nagesh <designer.nagesh@gmail.com>
2020-02-09 20:54:41 +0530

    Updates

---
+++ precache-manifest.195e64d09d21b311553c38189d127b79.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933c24c1afd6e8b4a28f5d9f1cdadd69",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "2f0a6bc3ea0edb562f05",
+    "url": "/project1/static/css/main.b020318f.chunk.css"
+  },
+  {
+    "revision": "8b45b327f1fd96267c8e",
+    "url": "/project1/static/js/2.a9f00ce4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project1/static/js/2.a9f00ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0a6bc3ea0edb562f05",
+    "url": "/project1/static/js/main.0393682c.chunk.js"
+  },
+  {
+    "revision": "96c98147192d82a30475",
+    "url": "/project1/static/js/runtime-main.024ca956.js"
+  },
+  {
+    "revision": "32bd9c41bfe60778832c76677bc77e7e",
+    "url": "/project1/static/media/assassin.32bd9c41.jpg"
+  },
+  {
+    "revision": "bc0be71b38c0170d07fd5a45a1f7e725",
+    "url": "/project1/static/media/blade_runner.bc0be71b.jpg"
+  },
+  {
+    "revision": "5c2f9eefd90075baebd0ffcbea170ba6",
+    "url": "/project1/static/media/greatest_showman.5c2f9eef.jpg"
+  },
+  {
+    "revision": "a72fc17970a2cd74fcc82c4aefa842fe",
+    "url": "/project1/static/media/io.a72fc179.jpg"
+  },
+  {
+    "revision": "a9ec468bfb2e0b752748f2d2314c10df",
+    "url": "/project1/static/media/iron_giant.a9ec468b.jpg"
+  },
+  {
+    "revision": "09b9cd905c2586b9c155e05db5a6bfb4",
+    "url": "/project1/static/media/joker.09b9cd90.jpg"
+  },
+  {
+    "revision": "a9101bd0c1f19abbcf9b6462f6527d52",
+    "url": "/project1/static/media/matrix.a9101bd0.jpg"
+  },
+  {
+    "revision": "7719385d8d860364d984532524756609",
+    "url": "/project1/static/media/posters.7719385d.jpg"
+  },
+  {
+    "revision": "5e331990a3dbd88c326280a1ae8983de",
+    "url": "/project1/static/media/replicas.5e331990.jpg"
+  },
+  {
+    "revision": "4d06f2f29e87661243ffbe4e94f96521",
+    "url": "/project1/static/media/rush.4d06f2f2.jpg"
+  },
+  {
+    "revision": "18890fe8d5f28a589ce643ebed8f5328",
+    "url": "/project1/static/media/wolverine.18890fe8.jpg"
+  }
+]);
